0c83bf274452c84dad88cfe775ebadeaf6555a53
Bruno Demétrio <bdemetriosantos@gmail.com>
2019-03-14 13:32:26 -0300

    Criação do projeto Spring

---
+++ src/main/java/br/com/dm3brain/BlogappApplication.java
@@ -0,0 +1,13 @@
+package br.com.dm3brain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogappApplication.class, args);
+	}
+
+}
