27e1edd762c4b054cef0a69b7349e717adf3fd3c
Bruno Demétrio Santos <bdemetriosantos@gmail.com>
2019-04-23 15:33:27 -0300

    Atualização do package do projeto para utilizar a nova versão da DBrain Studio.

--- src/main/java/br/com/dbrainstudio/BlogappApplication.java
+++ src/main/java/br/com/dbrainstudio/BlogappApplication.java
@@ -1,4 +1,4 @@
-package br.com.dm3brain;
+package br.com.dbrainstudio;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/br/com/dbrainstudio/post/PostResource.java
+++ src/main/java/br/com/dbrainstudio/post/PostResource.java
@@ -1,4 +1,4 @@
-package br.com.dm3brain.post;
+package br.com.dbrainstudio.post;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
