e98985210cb0eb461db9d976f73a22d68f61f955
Bruno Demétrio Santos <bdemetriosantos@gmail.com>
2019-04-24 15:18:49 -0300

    Inserção de anotação na interface BaseRepository para garantir que ela não seja um bean gerenciável do Spring, já que sua função é apenas servir como uma interface base para os demais repositórios.

--- src/main/java/br/com/dbrainstudio/entity/BaseRepository.java
+++ src/main/java/br/com/dbrainstudio/entity/BaseRepository.java
@@ -1,7 +1,9 @@
 package br.com.dbrainstudio.entity;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.NoRepositoryBean;
 
+@NoRepositoryBean
 public interface BaseRepository<T> extends JpaRepository<T, Long> {
 
 }
